b66599d5d5f45ce799da8e900e2b0d80af7c8c5f
Antriksh Sharma <antrikshsharma18102@gmail.com>
2022-10-14 23:29:20 +0530

    Updates

---
+++ precache-manifest.6c5bfb7f66ec3b3359d4cf7b52f6f31a.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c067b7ff3d1645a935337cf99a67b09",
+    "url": "/St4_project.github.io/react_upload/index.html"
+  },
+  {
+    "revision": "24b55e0579369ace584a",
+    "url": "/St4_project.github.io/react_upload/static/css/2.4e498853.chunk.css"
+  },
+  {
+    "revision": "2bc19d75be00acd9b6e4",
+    "url": "/St4_project.github.io/react_upload/static/css/main.ceaccd31.chunk.css"
+  },
+  {
+    "revision": "24b55e0579369ace584a",
+    "url": "/St4_project.github.io/react_upload/static/js/2.af8d619f.chunk.js"
+  },
+  {
+    "revision": "914a85462fc888825cb3a498c2b9869c",
+    "url": "/St4_project.github.io/react_upload/static/js/2.af8d619f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bc19d75be00acd9b6e4",
+    "url": "/St4_project.github.io/react_upload/static/js/main.91066c13.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/St4_project.github.io/react_upload/static/js/main.91066c13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d529e83efeaab46d05d7",
+    "url": "/St4_project.github.io/react_upload/static/js/runtime-main.3e45430c.js"
+  },
+  {
+    "revision": "31e1300d419245fd27614630601dc74d",
+    "url": "/St4_project.github.io/react_upload/static/media/bootstrap-icons.31e1300d.woff2"
+  },
+  {
+    "revision": "be6df3f0c9855796ae0c099ee4ca43ce",
+    "url": "/St4_project.github.io/react_upload/static/media/bootstrap-icons.be6df3f0.woff"
+  },
+  {
+    "revision": "cafe63d7b375efbc012eb1f1188b649d",
+    "url": "/St4_project.github.io/react_upload/static/media/ds.cafe63d7.jpeg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/St4_project.github.io/react_upload/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/St4_project.github.io/react_upload/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/St4_project.github.io/react_upload/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/St4_project.github.io/react_upload/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/St4_project.github.io/react_upload/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "d1454ac64a2af4b4df02c58ca8116eb2",
+    "url": "/St4_project.github.io/react_upload/static/media/mw.d1454ac6.jpg"
+  },
+  {
+    "revision": "654d54919e15895a6234694d8730e901",
+    "url": "/St4_project.github.io/react_upload/static/media/sales.654d5491.jpeg"
+  },
+  {
+    "revision": "01c29d53741b4327805f751188d591f3",
+    "url": "/St4_project.github.io/react_upload/static/media/tech3.01c29d53.jpeg"
+  }
+]);
